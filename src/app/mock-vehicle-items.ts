72041ffa8e1a8cbacafe0a814471e13fb19c5b4c
Chuck <cfunkles@gmail.com>
2017-10-31 10:13:40 -0600

    add mock vehicle items

---
+++ src/app/mock-vehicle-items.ts
@@ -0,0 +1,52 @@
+import{ VehicleItem } from './vehicle-item';
+
+export const VITEMS: VehicleItem[] = [
+    {
+        name: 'Samsung 65"LED Smart 4K Ultra HDTV',
+        munnies: 16,
+        weight: 90,
+        image: 'https://pisces.bbystatic.com/image2/BestBuy_US/images/products/5773/5773800_sd.jpg'
+    },
+    {
+        name: 'Queen Koil Mattress set',
+        munnies: 10,
+        weight: 8,
+        image: "http://generalplatform.blob.core.windows.net/wwwthemattressfactoryinccom/multimedia/images/brand-collection/hero/large/bc80d575-0223-4ab4-a563-bf65bf412a98.jpg"
+    },
+    {
+        name: 'Grill Master 5000',
+        munnies: 4,
+        weight: 95,
+        image: 'http://staceygustafson.com/wp-content/uploads/2013/06/Hemi-Grill.jpg'
+    },
+    {
+        name: 'Skittles',
+        munnies: 25,
+        weight: 80,
+        image: 'https://smedia.webcollage.net/rwvfp/wc/cp/21469538/module/wrigley/_cp/products/1452027131596/tab-56242735-f441-432c-aad5-c8fe7c421f32/resource-ef9c6f55-91a6-4901-86a2-bf9bf684945d.jpg'
+    },
+    {
+        name: '1969 Stingray Corvette',
+        munnies: 45,
+        weight: 3,
+        image: 'https://s-media-cache-ak0.pinimg.com/originals/16/66/08/166608387b644988b02b1da70dcfed12.jpg'
+    },
+    {
+        name: 'Silk Underwear',
+        munnies: 16,
+        weight: 70,
+        image: 'http://www.royalsilkusa.com/images/silk-boxers-sky-blue-stripes.jpg'
+    },
+    {
+        name: 'Confortable Concrete Shoes',
+        munnies: 0.5,
+        weight: 0.5,
+        image: 'https://www.concretedecor.net/CD/assets/Image/archives/CD1505/1505i_Page_26_Image_0003_600.jpg'
+    },
+    {
+        name: 'Dwayne Johnson THE ROCK!',
+        munnies: 1.75,
+        weight: 8.92,
+        image: 'http://www.southpawer.com/wp-content/uploads/2015/07/Dwayne-Johnson-eminem-gym-songs.jpg'
+    }
+]
